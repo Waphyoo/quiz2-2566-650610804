7d230c62b7a04df855e257447eabefbea44ac70a
Waphyoo <nonny2246@gmail.com>
2023-08-10 14:56:38 +0700

    ok1

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,26 @@
 export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/non.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          Watunyoo Phanapaisarnsakul 650610804
+        </span>
+      </div>
+
+      <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span className="text-muted">100 คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
--- src/libs/comments.js
+++ src/libs/comments.js
@@ -1,17 +1,20 @@
 export const comments = [
   {
+    id: 1,
     username: "Lisa",
     userImagePath: "/profileImages/lisa.jpg",
     commentText: "จริงค่า",
     likeNum: 999,
     replies: [
       {
+        id: 1,
         username: "หมาน้อย",
         userImagePath: "/profileImages/puppy.jpg",
         replyText: "จริงค้าบบบบบบบบ",
         likeNum: 0,
       },
       {
+        id: 2,
         username: "Cat Meme",
         userImagePath: "/profileImages/popcat.png",
         replyText: "ลิซ่าาาาาาา",
@@ -20,6 +23,7 @@ export const comments = [
     ],
   },
   {
+    id: 2,
     username: "Charlie Brown",
     userImagePath: "/profileImages/charliebrown.jpg",
     commentText: "บ้าไปแล้ว",
